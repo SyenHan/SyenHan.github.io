99b3a958155812df3fa750dfae8c03ddfa2f3775
Syen <syenhan@163.com>
2017-08-21 01:13:43 -0500

    Add files via upload

---
+++ annualbeta/js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$(document).ready(function(){ 
+
+$(".header").sticky({ topSpacing: 0 });
+
+/* Menu Anchors */
+$('a[href*=#]').click(function() {
+ if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+	 var $target = $(this.hash);
+	 $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+	 if ($target.length) {
+		 var targetOffset = $target.offset().top;
+		 $('html,body').animate({scrollTop: targetOffset-75}, 1000);
+		 return false;
+		}
+	}
+});
+
+
+});
